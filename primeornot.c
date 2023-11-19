6b2e92efe7527e98b82eaa79954ca260def1eb47
Amit Kushwaha <145186713+Amit72777@users.noreply.github.com>
2023-11-20 00:09:49 +0530

    time consuming

--- primeornot.c
+++ primeornot.c
@@ -6,14 +6,13 @@ int main()
     printf("Enter the number :");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= n/2+1; i++)
     {
         if (n % i == 0)
-        {
             a++;
-            if (a == 3)
-                break;
-        }
+        if (a == 3)
+            break;
+        
     }
     if (a == 2)
         printf("this is prime number ");
